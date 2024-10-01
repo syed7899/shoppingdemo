5087b4fb36d04c26302c55c04e81c23e2633662c
syed7899 <89896655+syed7899@users.noreply.github.com>
2024-10-01 11:19:24 +0530

    return List of products

--- src/main/java/com/easy/shop/shoppingdemo/controller/ShoppingController.java
+++ src/main/java/com/easy/shop/shoppingdemo/controller/ShoppingController.java
@@ -5,4 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ShoppingController {
 
+  @GetMapping("/products")
+  public List<String> getProducts(){
+    ArrayList<String> al = new ArrayList<>();
+    al.add("Refrdigerator");
+    al.add("Phone");
+    al.add("Television");
+    al.add("Laptop");
+    al.add("ipad");
+    return al;
+  }
+
 }
