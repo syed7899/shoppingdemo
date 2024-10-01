7bd7397107eeb44987e2e68c68b8b29ac4ca6f12
Syed <remindme7899@gmail.com>
2024-10-01 11:24:09 +0530

    returns prices of products

--- src/main/java/com/easy/shop/shoppingdemo/controller/ShoppingController.java
+++ src/main/java/com/easy/shop/shoppingdemo/controller/ShoppingController.java
@@ -1,8 +1,22 @@
 package com.easy.shop.shoppingdemo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShoppingController {
+	
+	
+	public Map<String,Integer> getPriceList(){
+		Map map= new HashMap<>();
+		map.put("Refrdigerator", 25000);
+		map.put("Phone", 15000);
+		map.put("Television", 22000);
+		map.put("Laptop", 35000);
+		map.put("ipad", 15000);
+		return map;
+	}
 
 }
