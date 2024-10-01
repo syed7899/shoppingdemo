c7cd5f858a8419f4a4ebb44397a60230e650fb2a
Syed <remindme7899@gmail.com>
2024-10-01 10:45:33 +0530

    Initial commit

---
+++ src/main/java/com/easy/shop/shoppingdemo/ShoppingdemoApplication.java
@@ -0,0 +1,13 @@
+package com.easy.shop.shoppingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easy/shop/shoppingdemo/ShoppingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.shop.shoppingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
